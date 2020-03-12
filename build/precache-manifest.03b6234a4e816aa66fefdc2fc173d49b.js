e8f119a8d91e76b1d159fca28000671d055f5a6a
acavalls <alejandro.canosa@bsc.es>
2020-03-12 12:07:31 +0000

    Aggregations: Removing hard-coded table fields

---
+++ build/precache-manifest.03b6234a4e816aa66fefdc2fc173d49b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4081adb73232aed7e9e684dc37e1bbf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a85efcc8c48ce4620f",
+    "url": "/static/css/3.55cc6826.chunk.css"
+  },
+  {
+    "revision": "333772931685f1f24d94",
+    "url": "/static/css/5.69d8c41a.chunk.css"
+  },
+  {
+    "revision": "36bfbb3e06991cf8548e",
+    "url": "/static/css/6.5b0e1c0d.chunk.css"
+  },
+  {
+    "revision": "b7d98dd4a56135683de4",
+    "url": "/static/css/7.3a220d04.chunk.css"
+  },
+  {
+    "revision": "167925f3b12dcabff768",
+    "url": "/static/css/8.f7cf8957.chunk.css"
+  },
+  {
+    "revision": "b9d4e4ed907f8de02179",
+    "url": "/static/css/9.6c5a005e.chunk.css"
+  },
+  {
+    "revision": "1ef001c472e8002ef688",
+    "url": "/static/css/main.aa661c8a.chunk.css"
+  },
+  {
+    "revision": "00a868c1a4096b2cd009",
+    "url": "/static/js/0.c217e4ae.chunk.js"
+  },
+  {
+    "revision": "83a85efcc8c48ce4620f",
+    "url": "/static/js/3.ed502d84.chunk.js"
+  },
+  {
+    "revision": "0df560b916996a62a87c",
+    "url": "/static/js/4.a8b6d338.chunk.js"
+  },
+  {
+    "revision": "333772931685f1f24d94",
+    "url": "/static/js/5.ba7e6407.chunk.js"
+  },
+  {
+    "revision": "36bfbb3e06991cf8548e",
+    "url": "/static/js/6.2566b41c.chunk.js"
+  },
+  {
+    "revision": "b7d98dd4a56135683de4",
+    "url": "/static/js/7.fd388824.chunk.js"
+  },
+  {
+    "revision": "167925f3b12dcabff768",
+    "url": "/static/js/8.f69abc14.chunk.js"
+  },
+  {
+    "revision": "b9d4e4ed907f8de02179",
+    "url": "/static/js/9.2377497b.chunk.js"
+  },
+  {
+    "revision": "1ef001c472e8002ef688",
+    "url": "/static/js/main.1af3d9d4.chunk.js"
+  },
+  {
+    "revision": "60fbd0b258f0daea7d4e",
+    "url": "/static/js/runtime-main.e6630089.js"
+  }
+]);
