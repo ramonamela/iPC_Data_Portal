f1fddce492ef331a6a8d88a554261fe4e24293d4
ramonamela <ramon.amela@gmail.com>
2020-05-20 18:41:27 +0200

    Remove keycloak

--- src/index.js
+++ src/index.js
@@ -2,48 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as Keycloak from 'keycloak-js';
+//import * as Keycloak from 'keycloak-js';
 
-let initOptions = {
-    url: 'https://inb.bsc.es/auth/', realm: 'IPC', clientId: 'ipc-react-portal', onLoad: 'login-required'
-}
 
-let keycloak = Keycloak(initOptions);
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
 
-keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
+// console.log("React");
 
-    if (!auth) {
-        window.location.reload();
-    } else {
-        console.info("Authenticated");
-    }
+ReactDOM.render(app, document.getElementById('root'));
 
-    const app = (
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    );
-
- // console.log("React");
-
-    ReactDOM.render(app, document.getElementById('root'));
-
-    localStorage.setItem("react-token", keycloak.token);
-    localStorage.setItem("react-refresh-token", keycloak.refreshToken);
-
-    setTimeout(() => {
-        keycloak.updateToken(70).success((refreshed) => {
-            if (refreshed) {
-                console.debug('Token refreshed' + refreshed);
-            } else {
-                console.warn('Token not refreshed, valid for '
-                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-            }
-        }).error(() => {
-            console.error('Failed to refresh token');
-        });
-    }, 60000)
-
-}).error(() => {
-    console.error("Authenticated Failed");
-});
